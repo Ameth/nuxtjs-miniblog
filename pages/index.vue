ee2c92fdfa0bc3da17bde5b2b32967503d65b1c5
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-10-24 22:29:42 -0500

    Static Site Generate

--- pages/index.vue
+++ pages/index.vue
@@ -19,27 +19,52 @@ import ArticleCard from '../components/ArticleCard.vue'
 import _idVue from './_id.vue'
 export default {
   name: 'IndexPage',
-  data () {
-    return {
-      articles: []
-    }
-  },
-  async mounted () {
+  // data () {
+  //   return {
+  //     articles: []
+  //   }
+  // },
+  async asyncData ({ isDev, $http }) {
     const baseUrl =
-      location.hostname === 'localhost'
+      isDev === true
         ? 'http://localhost:9999'
         : 'https://miniblog-nuxt-ameth.netlify.app'
     const url = `${baseUrl}/.netlify/functions/articles`
-    const { articles } = await this.$http.$get(url)
-    this.articles = articles.map(item => {
-      return {
-        ...item,
-        author: item['author-name'][0],
-        date: new Date(item.updated),
-        cover: 'https://picsum.photos/300/200'
-      }
-    })
+    const { articles } = await $http.$get(url)
+    return {
+      posts: articles
+    }
   },
+  computed: {
+    articles () {
+      return Array.isArray(this.posts)
+        ? this.posts.map(item => {
+            return {
+              ...item,
+              author: item['author-name'][0],
+              date: new Date(item.updated),
+              cover: 'https://picsum.photos/300/200'
+            }
+          })
+        : []
+    }
+  },
+  // async mounted () {
+  //   const baseUrl =
+  //     location.hostname === 'localhost'
+  //       ? 'http://localhost:9999'
+  //       : 'https://miniblog-nuxt-ameth.netlify.app'
+  //   const url = `${baseUrl}/.netlify/functions/articles`
+  //   const { articles } = await this.$http.$get(url)
+  //   this.articles = articles.map(item => {
+  //     return {
+  //       ...item,
+  //       author: item['author-name'][0],
+  //       date: new Date(item.updated),
+  //       cover: 'https://picsum.photos/300/200'
+  //     }
+  //   })
+  // },
   components: { About, ArticleCard }
 }
 </script>
